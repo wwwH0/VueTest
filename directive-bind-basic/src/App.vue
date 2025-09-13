6b48ca61a3762f998113f371824b8cd77623aacb
wwwH0 <229433520+wwwH0@users.noreply.github.com>
2025-09-13 16:23:19 +0900

    4.1.4

--- directive-bind-basic/src/App.vue
+++ directive-bind-basic/src/App.vue
@@ -1,11 +1,21 @@
-<script setup lang="ts"></script>
+`<script setup lang="ts">
+import { ref } from 'vue';
+
+const url = ref("https://vuejs.org");
+const isSendButtonDisabled = ref(true);
+const widthOrHeight = ref("height");
+const widthOrHeightValue = ref(100);
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <p><a v-bind:href="url" target="_blank">Vue.js Site</a></p>
+  <p><a :href="url" target="_blank">Vue.js Site 省略系</a></p>
+  <p><a v-bind:href="url + '/guide/introduction.html'" target="_blank">Vue.js Guide</a></p>
+  <p><button type="button" v-bind:disabled="isSendButtonDisabled">送信</button></p>
+
+  <p><img alt="VueLogo" src="./assets/logo.svg" v-bind:[widthOrHeight]="widthOrHeightValue"></p>
+
 </template>
 
 <style scoped></style>
+`
