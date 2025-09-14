27430251b103ad3a5a889413404003dcd85cbe95
wwwH0 <229433520+wwwH0@users.noreply.github.com>
2025-09-14 20:22:49 +0900

    init 4.2

---
+++ directive-on/src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const randValue = ref("not yet!");
+const onButtonClick = (): void => {
+  const rand = Math.round(Math.random() * 10);
+  randValue.value = String(rand);
+};
+</script>
+
+<template>
+  <section>
+    <button v-on:click="onButtonClick">Click!!</button>
+    <p>Result of the click : {{ randValue }}</p>
+  </section>
+</template>
+
+<style scoped></style>
