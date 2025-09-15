22207d07d8639865c2d4b466ecf7d73722b9a95a
wwwH0 <229433520+wwwH0@users.noreply.github.com>
2025-09-15 17:20:11 +0900

    4.2.3

--- directive-on/src/App.vue
+++ directive-on/src/App.vue
@@ -6,12 +6,23 @@ const onButtonClick = (): void => {
   const rand = Math.round(Math.random() * 10);
   randValue.value = String(rand);
 };
+
+const mousePointerX = ref(0);
+const mousePointerY = ref(0);
+const onImgMousemove = (event: MouseEvent): void => {
+  mousePointerX.value = event.offsetX;
+  mousePointerY.value = event.offsetY;
+}
 </script>
 
 <template>
   <section>
     <button v-on:click="onButtonClick">Click!!</button>
     <p>Result of the click : {{ randValue }}</p>
+  </section>
+  <section>
+    <img src="./assets/logo.svg" alt="Vue logo" width="200" v-on:mousemove="onImgMousemove"></img>
+    <p>location of pointer: x={{ mousePointerX }} y={{ mousePointerY }}</p>
   </section>
 </template>
 
