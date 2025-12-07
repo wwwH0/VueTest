01ce4381ea08f362efc050c6b6f8f5823ecf38ce
wwwH0 <229433520+wwwH0@users.noreply.github.com>
2025-12-07 17:07:53 +0900

    5.1.1

---
+++ directive-model/src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+  import { ref } from "vue";
+
+  // 5.1.1
+  const inputNameModel = ref("双方向");
+</script>
+
+<template>
+    <!-- 5.1.1 -->
+  <section>
+    <input type="text" v-model="inputNameModel"></input>
+    <p>{{ inputNameModel }}</p>
+  </section>
+</template>
+
+<style scoped></style>
