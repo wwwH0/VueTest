33756fb12ca18a88757124ae0b01f998f905640c
wwwH0 <229433520+wwwH0@users.noreply.github.com>
2025-12-07 17:24:12 +0900

    5.1.2

--- directive-model/src/App.vue
+++ directive-model/src/App.vue
@@ -3,6 +3,14 @@
 
   // 5.1.1
   const inputNameModel = ref("双方向");
+
+  // 5.1.2
+  const inputNameBind = ref("しんちゃん");
+  const inputNameOn = ref("ななし");
+  const onInputName = (event: Event): void => {
+    const element = event.target as HTMLInputElement;
+    inputNameOn.value = element.value;
+  }
 </script>
 
 <template>
@@ -11,6 +19,16 @@
     <input type="text" v-model="inputNameModel"></input>
     <p>{{ inputNameModel }}</p>
   </section>
+
+  <!-- 5.1.2 -->
+   <section>
+    <input type="text" v-bind:value="inputNameBind"></input>
+   </section>
+   <br></br>
+   <section>
+    <input type="text" v-on:input="onInputName"></input>
+    <p>{{ inputNameOn }}</p>
+   </section>
 </template>
 
 <style scoped></style>
