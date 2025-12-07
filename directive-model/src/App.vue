09862fbae1d3e24dc4c18e63317ffcde9e1aa04f
wwwH0 <229433520+wwwH0@users.noreply.github.com>
2025-12-07 17:39:37 +0900

    5.1.3

--- directive-model/src/App.vue
+++ directive-model/src/App.vue
@@ -11,17 +11,26 @@
     const element = event.target as HTMLInputElement;
     inputNameOn.value = element.value;
   }
+
+  // 5.1.3
+  const inputName2way = ref("interactive");
+  const onInputName2way = (event: Event): void => {
+    const element = event.target as HTMLInputElement;
+    inputName2way.value = element.value;
+  }
 </script>
 
 <template>
     <!-- 5.1.1 -->
   <section>
+    <p>5.1.1</p>
     <input type="text" v-model="inputNameModel"></input>
     <p>{{ inputNameModel }}</p>
   </section>
 
   <!-- 5.1.2 -->
    <section>
+    <p>5.1.2</p>
     <input type="text" v-bind:value="inputNameBind"></input>
    </section>
    <br></br>
@@ -29,6 +38,13 @@
     <input type="text" v-on:input="onInputName"></input>
     <p>{{ inputNameOn }}</p>
    </section>
+
+   <!-- 5.1.3 -->
+    <section>
+      <p>5.1.3</p>
+      <input type="text" v-bind:value="inputName2way" v-on:input="onInputName2way"></input>
+      <p>{{ inputName2way }}</p>
+    </section>
 </template>
 
 <style scoped></style>
