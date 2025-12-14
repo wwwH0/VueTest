dd8aaf4f7b36b48416455133f7f1842908e80d55
wwwH0 <229433520+wwwH0@users.noreply.github.com>
2025-12-14 17:06:16 +0900

    6.1.1-6.1.3

---
+++ directive-control/src/App.vue
@@ -0,0 +1,59 @@
+<script setup lang="ts">
+  import {computed, ref} from "vue";
+
+//6.1.1
+const number =ref(80);
+const showOrNot = computed(
+  (): boolean => {
+    let showOrNot = false;
+    const rand = Math.round(Math.random() * 100);
+    if(rand >= 50){
+      showOrNot = true;
+    }
+    return showOrNot;
+  }
+);
+
+//6.1.2,6.1.3
+const randomNumber = computed (
+  (): number => {
+    return Math.round(Math.random() * 100);
+  }
+);
+</script>
+
+<template>
+  <p>6.1.1</p>
+  <p v-if="number >= 50">
+    条件に合致したので表示1️⃣
+  </p>
+  <p v-if="Math.round(Math.random() * 100) >= 50">
+    条件に合致したので表示2️⃣
+  </p>
+  <p v-if="showOrNot">
+    条件に合致したので表示3️⃣
+  </p>
+  <br>
+  <p>6.1.2</p>
+  <p>
+    点数は{{ randomNumber }}点で
+    <span v-if="randomNumber >= 80">優です。</span>
+    <span v-else-if="randomNumber >= 70">良です。</span>
+    <span v-else-if="randomNumber >= 60">可です。</span>
+    <span v-else>不可です。</span>
+  </p>
+  <br>
+  <p>6.1.3</p>
+  <p>
+    点数は{{ randomNumber }}点で
+    <template v-if="randomNumber >= 80">
+      優です。
+      <span style="color: red;">すばらしい！</span>
+    </template>
+    <span v-else-if="randomNumber >= 70">良です。</span>
+    <span v-else-if="randomNumber >= 60">可です。</span>
+    <span v-else>不可です。</span>
+  </p>
+</template>
+
+<style scoped></style>
