40b3234b1b7b8021dbcb224472fe0aadc8e82504
wwwH0 <229433520+wwwH0@users.noreply.github.com>
2025-12-30 16:17:29 +0900

    7.1.2

--- script-block/src/App.vue
+++ script-block/src/App.vue
@@ -1,39 +1,45 @@
 <script setup lang="ts">
-  import {ref, computed} from "vue";
+  //7.2
+  import {ref, watch, watchEffect} from "vue";
 
-  //7.1
+  const cocktailNo = ref(1);
+  const priceMsg = ref("");
+
+  watchEffect(
+    (): void => {
+      priceMsg.value = getCocktailInfo(cocktailNo.value);
+    }
+  );
+
+//cocktailNoを1秒ごとに1〜4の乱数を使って変更。
+setInterval(
+	():void => {
+		cocktailNo.value = Math.round(Math.random() * 3) + 1;
+	},
+	1000
+);
+
   interface Cocktail {
     id: number;
     name: string;
     price: number;
   }
-  const cocktailDataListInit = new Map<number, Cocktail>();
-  cocktailDataListInit.set(1, {id: 1, name: "ホワイトレディ", price: 1200});
-  cocktailDataListInit.set(2, {id: 2, name: "ブルーハワイ", price: 1500});
-  cocktailDataListInit.set(3, {id: 3, name: "ニューヨーク", price: 1100});
-  cocktailDataListInit.set(4, {id: 4, name: "マティーニ", price: 1500});
-  
-  const cocktailNo = ref(1);
-  const priceMsg = computed(
-    (): string => {
-      const cocktail = cocktailDataListInit.get(cocktailNo.value);
-      let msg = "該当カクテルはありません";
 
-      if(cocktail != undefined){
-        msg = `該当するカクテルは${cocktail.name}で、価格は${cocktail.price}円です。`;
-        //msg = '該当するカクテルは${cocktail.name}で、価格は${cocktail.price}円です。';
-      }
-      return msg;
+  function getCocktailInfo(cocktailNo: number): string {
+    const cocktailDataListInit = new Map<number, Cocktail>();
+    cocktailDataListInit.set(1, {id: 1, name: "ホワイトレディ", price: 1200});
+    cocktailDataListInit.set(2, {id: 2, name: "ブルーハワイ", price: 1500});
+    cocktailDataListInit.set(3, {id: 3, name: "ニューヨーク", price: 1100});
+    cocktailDataListInit.set(4, {id: 4, name: "マティーニ", price: 1500});
+    
+    const cocktail = cocktailDataListInit.get(cocktailNo);
+    let msg = "該当カクテルはありません";
+
+    if(cocktail != undefined){
+      msg = `該当するカクテルは${cocktail.name}で、価格は${cocktail.price}円です。`;
     }
-  );
-
-  setInterval(
-    (): void => {
-      cocktailNo.value = Math.round(Math.random() * 3) + 1;
-    },
-    1000
-  );
-
+    return msg;
+  }
 </script>
 
 <template>
